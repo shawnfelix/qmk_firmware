f792ba5a7f3b9e05bfd27f2f852669613dfbf718
Shawn <shawn.felix.cs@gmail.com>
2024-05-11 22:15:39 -0400

    match layout with kle

--- keyboards/phelix/phelixzero/keymaps/default/keymap.c
+++ keyboards/phelix/phelixzero/keymaps/default/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     */
     [_DEFAULT] = LAYOUT(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,   KC_6,       KC_7,   KC_8,  KC_9,    KC_0,   KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,               KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,               KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,               KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,               KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,     KC_DEL,
         KC_LCTL, KC_LGUI, KC_NO,   KC_LALT, KC_SPC,                       KC_SPC, MO(_UI_CONTROL), KC_RALT, MO(_UI_CONTROL),  KC_LEFT, KC_DOWN, KC_RGHT,
-        KC_NO,                                                            PB_1,   PB_2,  PB_3,    PB_4,   PB_5, PB_6
+                                                                          PB_1,   PB_2,  PB_3,    PB_4,   PB_5
     ),
     /*
     +--------+------+------+------+------+------+------+      +------+------+------+------+------+------+-------------+
@@ -70,9 +70,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD, PB_5,    PB_6,    KC_NO,    KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO,   KC_NO,   UI_UP,   KC_NO,    KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO,   UI_LEFT, UI_DOWN, UI_RIGHT, KC_NO, KC_NO,            KC_NO, UI_J,  UI_K,  UI_L,  KC_NO, KC_NO, KC_NO,
-        KC_NO,   DB_TOGG,   KC_VOLU,   KC_VOLD,    KC_NO, KC_NO,            KC_NO, UI_M,  UI_N,  KC_NO, PB_5, KC_NO, KC_NO, KC_NO,
-        KC_NO,   KC_NO,   QK_BOOT, KC_NO,    KC_NO,                 KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO,                                                      PB_1,   PB_2, PB_3,    PB_4,   PB_5, PB_6
+        KC_NO,   DB_TOGG,   KC_VOLU,   KC_VOLD,    KC_NO, KC_NO,      KC_NO, UI_M,  UI_N,  KC_NO, PB_5, KC_NO, KC_NO, KC_NO,
+        KC_NO,   KC_NO,   QK_BOOT, KC_NO,    KC_NO,                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                                                      PB_1,   PB_2, PB_3,    PB_4,   PB_5
     )
 };
 
